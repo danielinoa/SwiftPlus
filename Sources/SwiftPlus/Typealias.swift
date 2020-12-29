b63bd01cebd153682a345aed4f3086e5c4d0ef5b
Daniel Inoa <danielinoa@gmail.com>
2020-12-29 04:23:34 -0800

    Add typealias

---
+++ Sources/SwiftPlus/Typealias.swift
@@ -0,0 +1,8 @@
+//
+//  Created by Daniel Inoa on 12/29/20.
+//
+
+typealias Closure = () -> Void
+typealias Callback<T> = (T) -> Void
+typealias Factory<T> = () -> T
+typealias Map<T, U> = (T) -> U
