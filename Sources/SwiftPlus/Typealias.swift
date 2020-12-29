1c72a0ad32c0a2f7dd589816f256d4c338df6513
Daniel Inoa <danielinoa@gmail.com>
2020-12-29 04:27:23 -0800

    Make typealiases public

--- Sources/SwiftPlus/Typealias.swift
+++ Sources/SwiftPlus/Typealias.swift
@@ -2,7 +2,7 @@
 //  Created by Daniel Inoa on 12/29/20.
 //
 
-typealias Closure = () -> Void
-typealias Callback<T> = (T) -> Void
-typealias Factory<T> = () -> T
-typealias Map<T, U> = (T) -> U
+public typealias Closure = () -> Void
+public typealias Callback<T> = (T) -> Void
+public typealias Factory<T> = () -> T
+public typealias Map<T, U> = (T) -> U
