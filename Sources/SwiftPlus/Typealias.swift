87b1ee65a9dd325a9456899589bc701f7fdf1f09
Daniel Inoa <danielinoa@gmail.com>
2021-01-02 20:42:07 -0800

    Document typealiases

--- Sources/SwiftPlus/Typealias.swift
+++ Sources/SwiftPlus/Typealias.swift
@@ -2,7 +2,14 @@
 //  Created by Daniel Inoa on 12/29/20.
 //
 
+/// () -> T
 public typealias Factory<T> = () -> T
+
+/// () -> Void
 public typealias Closure = Factory<Void>
+
+/// (T) -> U
 public typealias Map<T, U> = (T) -> U
+
+/// (T) -> Void
 public typealias Callback<T> = Map<T, Void>
