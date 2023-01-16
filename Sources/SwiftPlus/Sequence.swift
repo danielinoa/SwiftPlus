7f6e04def2a6ffce7df93d3c1bd0d8ffb340743b
Daniel Inoa <danielinoa@gmail.com>
2023-01-16 11:19:29 -0800

    Add compactCast on Sequence

--- Sources/SwiftPlus/Sequence.swift
+++ Sources/SwiftPlus/Sequence.swift
@@ -5,7 +5,21 @@
 public extension Sequence {
 
     /// Returns an array containing the non-nil elements.
+    /// # Example
+    /// ```
+    /// let numbers: [Int] = [1, nil, 2, nil, 3].compact() // returns [1, 2, 3]
+    /// ```
     func compact<T>() -> [T] where Element == Optional<T> {
         compactMap { $0 }
+    }
+
+    /// Returns an array containing the elements that can be casted to the specified type.
+    /// # Example
+    /// ```
+    /// let elements: [Any] = [1, "abc", 2, true, 3]
+    /// let integers: [Int] = elements.compactCast(to: Int.self) // returns [1, 2, 3]
+    /// ```
+    func compactCast<T>(to type: T.Type = T.self) -> [T] {
+        compactMap { $0 as? T }
     }
 }
--- Tests/SwiftPlusTests/SwiftPlusTests.swift
+++ Tests/SwiftPlusTests/SwiftPlusTests.swift
@@ -4,13 +4,21 @@ import XCTest
 final class SwiftPlusTests: XCTestCase {
 
     func testSequenceCompact() {
-        let optionalNumbers: [Int?] = [1, nil, 2, nil, 3]
-        let results: [Int] = optionalNumbers.compact()
+        let elements: [Int?] = [1, nil, 2, nil, 3]
+        let results: [Int] = elements.compact()
         let expected: [Int] = [1, 2, 3]
         XCTAssertEqual(results, expected)
     }
 
+    func testSequenceCompactCast() {
+        let elements: [Any] = [1, "abc", 2, true, 3]
+        let result: [Int] = elements.compactCast()
+        let expected = [1, 2, 3]
+        XCTAssertEqual(result, expected)
+    }
+
     static var allTests = [
         ("testSequenceCompact", testSequenceCompact),
+        ("testSequenceCompactCast", testSequenceCompactCast)
     ]
 }
