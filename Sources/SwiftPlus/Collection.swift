dd6d5558383b634cbd4d8f05d19512b3d6ed3911
Daniel Inoa <danielinoa@gmail.com>
2023-01-26 19:17:21 -0800

    Remove empty

--- Sources/SwiftPlus/Collection.swift
+++ Sources/SwiftPlus/Collection.swift
@@ -4,9 +4,6 @@
 
 public extension Collection {
 
-    /// Returns a new empty collection.
-    static var empty: [Element] { .init() }
-
     /// Returns `true` if the collection is not empty.
     var hasElements: Bool { !isEmpty }
 }
