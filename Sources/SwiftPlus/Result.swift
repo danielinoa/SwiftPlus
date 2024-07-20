6ec444659ef9444ae757c2fd20953d44d36fe230
Daniel Inoa <danielinoa@gmail.com>
2024-07-19 20:18:00 -0700

    Add convenience extension to handle success case

--- Sources/SwiftPlus/Result.swift
+++ Sources/SwiftPlus/Result.swift
@@ -2,10 +2,27 @@
 //  Created by Daniel Inoa on 3/9/23.
 //
 
+public extension Result {
+
+    @discardableResult
+    func ifSuccess(_ callback: (Success) -> Void) -> Result {
+        guard case .success(let value) = self else { return self }
+        callback(value)
+        return self
+    }
+}
+
 public extension Result where Success == Void {
 
     /// A void success.
     static var success: Result {
         .success(())
+    }
+
+    @discardableResult
+    func ifVoidSuccess(_ closure: () -> Void) -> Self {
+        guard case .success = self else { return self }
+        closure()
+        return self
     }
 }
